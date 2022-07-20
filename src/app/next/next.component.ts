ec5a4f842e2e5f51c0751002efbcfa4b2da6c86b
Evgeniy Gorbunov <evgeniy.gorbunov@dialmyapp.com>
2022-07-20 14:54:37 +0300

    d

---
+++ src/app/next/next.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-next',
+  templateUrl: './next.component.html',
+  styleUrls: ['./next.component.scss']
+})
+export class NextComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  back(){
+    this.router.navigate(['']);
+  }
+}
